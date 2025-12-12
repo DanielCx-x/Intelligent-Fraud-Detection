b1fd633e646cb64c03de3edf9b59387dee59bc93
DanielCx-x <zhhchendaniel@gmail.com>
2025-12-12 16:56:46 +0800

    Update model.py

--- src/model.py
+++ src/model.py
@@ -34,33 +34,3 @@ class Autoencoder(nn.Module):
 
     def forward(self, x):
         return self.decoder(self.encoder(x))
-
-class VAE(nn.Module):
-    def __init__(self, input_dim):
-        super(VAE, self).__init__()
-        # Encoder
-        self.fc1 = nn.Linear(input_dim, 32)
-        self.fc2_mu = nn.Linear(32, 8)      # mean μ
-        self.fc2_logvar = nn.Linear(32, 8)  # log(σ^2)
-        # Decoder
-        self.fc3 = nn.Linear(8, 32)
-        self.fc4 = nn.Linear(32, input_dim)
-        self.relu = nn.ReLU()
-
-    def encode(self, x):
-        h1 = self.relu(self.fc1(x))
-        return self.fc2_mu(h1), self.fc2_logvar(h1)
-
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-        return mu + eps * std
-
-    def decode(self, z):
-        h3 = self.relu(self.fc3(z))
-        return self.fc4(h3)
-
-    def forward(self, x):
-        mu, logvar = self.encode(x)
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
